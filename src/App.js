0a1fc0de016b64d5820c1fb1effd38c62770aa58
Spengs <Jotto465@gmail.com>
2018-06-12 21:44:32 -0500

    display data from server

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import './App.css';
 
 class App extends Component {
   state = {
-    response: ''
+    response: []
   }
 
   componentDidMount() {
     axios.get('/foo').then(res => {
-      console.log('response', res)
+      console.log('response', res.data)
+      this.setState({response: res.data})
     })
   }
   render() {
     return (
       <div className="App">
-
+        <ul>
+          {this.state.response.map(res => <li>{res}</li>)}
+        </ul>
       </div>
     );
   }
